f8ba8c1f33e925cce83936cf9b5ad34df195b812
adi33tyan <21eg107a272anurag.edu.in>
2023-08-09 11:08:21 +0530

    main

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain=require('../controllers/main');
+const ctrlLocations=require('../controllers/locations');
+const ctrlOthers=require('../controllers/others');
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location1', ctrlLocations.locationInfo1);
+router.get('/location2', ctrlLocations.locationInfo2);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/about',ctrlOthers.about);
+router.get('/signin',ctrlOthers.signin);
+router.get('/register',ctrlOthers.register);
 module.exports = router;
